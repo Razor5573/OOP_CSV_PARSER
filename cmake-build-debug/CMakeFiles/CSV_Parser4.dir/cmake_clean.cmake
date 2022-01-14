77840954937aaee3cf9557e712fd3b743c290ebf
Никита Калугин <nikitakalugin02@gmail.com>
2022-01-14 21:51:21 +0700

    CSV_Parser
    CSV_Parser

---
+++ cmake-build-debug/CMakeFiles/CSV_Parser4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSV_Parser4.dir/main.cpp.obj"
+  "CSV_Parser4.exe"
+  "CSV_Parser4.exe.manifest"
+  "CSV_Parser4.pdb"
+  "libCSV_Parser4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSV_Parser4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CSV_Parser4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CSV_Parser4.
+# This may be replaced when dependencies are built.
